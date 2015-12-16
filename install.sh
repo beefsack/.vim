b435ca0fb4688ce0db7e1b54611aac66af8e582b
Michael Alexander <beefsack@gmail.com>
2015-12-17 09:24:22 +1100

    Conditionally clone neobundle

--- install.sh
+++ install.sh
@@ -2,4 +2,6 @@
 
 curl https://raw.githubusercontent.com/beefsack/.vim/master/.vimrc > ~/.vimrc
 mkdir -p ~/.vim/bundle
-git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+if [ ! -d "~/.vim/bundle/neobundle.vim" ]; then
+  git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+fi
